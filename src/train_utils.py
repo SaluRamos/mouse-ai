9e42b4f8c322b0ea9f38255442ee1b0a97fdb68a
Salu Ramos <61796219+SaluRamos@users.noreply.github.com>
2026-01-17 02:47:35 -0300

    utils.py

--- src/train_utils.py
+++ src/train_utils.py
@@ -1,21 +1,11 @@
+#modules
+from src.utils import calc_degree_btw_vecs, calc_vec_magnitude
 #libs
 import tensorflow as tf
 import numpy as np
 #native libs
-import math
 import csv
 import glob
-
-def calc_vec_magnitude(v:list) -> float:
-    return math.sqrt(v[0]**2 + v[1]**2)
-
-def calc_degree_btw_vecs(v1:list, v2:list) -> float:
-    dot = v1[0]*v2[0] + v1[1]*v2[1]
-    mag1 = math.sqrt(v1[0]**2 + v1[1]**2)
-    mag2 = math.sqrt(v2[0]**2 + v2[1]**2)
-    if mag1 == 0 or mag2 == 0: return 0
-    cos_theta = max(-1, min(1, dot / (mag1 * mag2)))
-    return math.degrees(math.acos(cos_theta))
 
 def load_csv_data(filter_mov0:bool, filter_big_degree:bool):
     X = []
---
+++ src/utils.py
@@ -0,0 +1,12 @@
+import math
+
+def calc_vec_magnitude(v:list) -> float:
+    return math.sqrt(v[0]**2 + v[1]**2)
+
+def calc_degree_btw_vecs(v1:list, v2:list) -> float:
+    dot = v1[0]*v2[0] + v1[1]*v2[1]
+    mag1 = math.sqrt(v1[0]**2 + v1[1]**2)
+    mag2 = math.sqrt(v2[0]**2 + v2[1]**2)
+    if mag1 == 0 or mag2 == 0: return 0
+    cos_theta = max(-1, min(1, dot / (mag1 * mag2)))
+    return math.degrees(math.acos(cos_theta))
